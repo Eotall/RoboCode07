645d618e34b0d5d5b8dec2f7591b9c948b0148aa
Emil Gedenryd <emil.gedenryd@gmail.com>
2019-04-01 11:50:10 +0200

    Lade till följande klasser:
    findBots
    findOpponents
    findTeammates
    shouldEnterDefensive
    findLeader
    orderTeamToAttack
    finalWords
    enterDefensiveposition
    monveTeammates

--- src/grupp07/Specification.java
+++ src/grupp07/Specification.java
@@ -1,5 +1,40 @@
 package grupp07;
 
+import java.util.ArrayList;
+
+import robocode.Robot;
+
 public interface Specification {
+
+	/* Returnerar en lista med alla Robotar på planen */
+	ArrayList<Robot> findBots();
+
+	/* Returnerar en Arraylist med alla motståndare */
+	ArrayList<Robot> findOpponents(ArrayList<Robot> allBots);
+
+	/* Returnerar en lista med alla lagmedlemmar */
+	ArrayList<Robot> findTeammates(ArrayList<Robot> allBots);
+
+	/* Kontrollerar om laget ska gå in i defensivt läge */
+	boolean shouldEnterDefensive();
+
+	/* Returnerar ledaren i det andra laget */
+	Robot findLeader(ArrayList<Robot> allEnemies);
+
+	/* Säger åt laget att attackera en motståndare */
+	void orderTeamToAttack(Robot opponent);
+
+	/*
+	 * Skickar ett sista kommando till alla lagmedlemmar om ledaren håller på att dö
+	 */
+	void finalWords();
+
+	/* Säger åt laget att gå in i en defensiv position */
+	void enterDefensiveposition();
+	
+	/* Säger åt teammates att röra sig till koordinater */
+	void moveTeammates();
+	
+	
 
 }
