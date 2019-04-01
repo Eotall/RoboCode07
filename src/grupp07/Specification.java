a09b928408d136409ed67863d3b506cfafa0feaa
Emil Gedenryd <emil.gedenryd@gmail.com>
2019-04-01 17:10:58 +0200

    Började anpassa specifikation efter droid

--- src/grupp07/Specification.java
+++ src/grupp07/Specification.java
@@ -1,44 +1,23 @@
 package grupp07;
 
-import java.util.ArrayList;
-
 import robocode.Robot;
 
 public interface Specification {
 
-	/* Returnerar en lista med alla Robotar på planen */
-	ArrayList<Robot> findBots();
-
-	/* Returnerar en Arraylist med alla motståndare */
-	ArrayList<Robot> findOpponents(ArrayList<Robot> allBots);
-
-	/* Returnerar en lista med alla lagmedlemmar */
-	ArrayList<Robot> findTeammates(ArrayList<Robot> allBots);
-
-	/*
-	 * Kontrollerar om laget ska gå in i defensivt läge, exempelvis om energin blir
-	 * för låg
-	 */
-	boolean shouldEnterDefensive();
-
-	/* Returnerar ledaren i det andra laget */
-	Robot findLeader(ArrayList<Robot> allEnemies);
-
-	/* Säger åt laget att attackera en motståndare */
-	void orderTeamToAttack(Robot opponent);
-
-	/*
-	 * Skickar ett sista kommando till alla lagmedlemmar om ledaren håller på att dö
-	 */
-	void finalWords();
-
-	/* Säger åt laget att gå in i en defensiv position */
-	void enterDefensiveposition();
-
-	/* Säger åt teammates att röra sig till koordinater */
-	void moveTeammates();
-
-	/* Attackerar en motståndare */
-	void attack(Robot opponent);
-
+	/*Agerar på meddelanden som tas emot*/
+	void onMessageRecieved(robocode.MessageEvent e);
+	
+	void shoot(int x, int y);
+	
+	void shoot(int grader);
+	
+	void changeColor();
+	
+	void moveToCenter();
+	
+	void onHit();
+	
+	
+	
+	
 }
